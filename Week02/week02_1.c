1a72702bf70650c0699a5ae8c0c80f63297e10fe
whale5204 <tunatune667@gmail.com>
2024-10-02 14:11:48 +0900

    week02

--- Week02/week02_1.c
+++ Week02/week02_1.c
@@ -5,14 +5,12 @@
 
 int main(int argc, char *argv[]) 
 {
-	int a;
-	int b;
+	int a, b;
 	
 	a = 10;
 	b = 20;
 	
 	printf("Hello, Sookmyung Electronics!\n");
-	
 	printf("%d + %d = %d\n", a, b, a+b);
 	
 	return 0;
