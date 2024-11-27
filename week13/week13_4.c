bcdc2f44cf55e790ec1b2d94fbfa2a240214aa59
whale5204 <tunatune667@gmail.com>
2024-11-27 15:16:34 +0900

    week13

--- week13/week13_4.c
+++ week13/week13_4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -9,12 +10,13 @@ struct point
 	int y;
 };
 
+//pointer
 int getArea(struct point *pPtr1, struct point *pPtr2)
 {
 	int xdiff, ydiff, result;
 	
-	xdiff = (pPtr1->x) - (pPtr2->x);
-	ydiff = (pPtr1->y) - (pPtr2->y);
+	xdiff = fabs((pPtr1->x) - (pPtr2->x));
+	ydiff = fabs((pPtr1->y) - (pPtr2->y));
 	result = (xdiff)*(ydiff);
 	
 	return result;
@@ -39,3 +41,37 @@ int main(int argc, char *argv[])
 	
 	return 0;
 }
+
+/*
+int getArea(struct point p1, struct point p2)
+{
+	int xdiff, ydiff, result;
+	
+	xdiff = fabs((p1.x) - (p2.x));
+	ydiff = fabs((p1.y) - (p2.y));
+	result = (xdiff)*(ydiff);
+	
+	return result;
+}
+
+int main(int argc, char *argv[]) 
+{
+	struct point p1, p2;
+	struct point *pPtr1, *pPtr2;
+	int area;
+	
+	pPtr1 = &p1, pPtr2 = &p2;
+	
+	printf("Input the coordinate p1 (x y): ");
+	scanf("%d %d", &p1.x, &p1.y);
+	printf("Input the coordinate p2 (x y): ");
+	scanf("%d %d", &p2.x, &p2.y);
+	
+	area = getArea(p1, p2);
+	
+	printf("Area: %d", area);
+	
+	return 0;
+}
+*/
+
